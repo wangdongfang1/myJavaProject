14d241b5dd7fb72e5e9b1d05d554719fcfe4ee08
wangdongfang1 <w1021468165@163.com>
2022-08-09 13:50:09 +0800

    IDEA测试JAVAAWEB

---
+++ src/com/demo/Main.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.printf("阿斯顿发射点利夫卡是代理费78");
+        int numb =0;
+        System.out.println("numb = " + numb);
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("Main.main");
+        System.out.println(numb);
+
+    }
+}
